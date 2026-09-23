484f15ca715cc19f01e0b7f7bcf3e4073511b1c7
Per Sandberg <per.s.sandberg@bahnhof.se>
2026-09-23 07:48:50 +0200

    Reformat

--- check-tag-and-tag-if-allowed.py
+++ check-tag-and-tag-if-allowed.py
@@ -2,41 +2,41 @@ import subprocess
 import sys
 
 
-version=subprocess.check_output(["bin/helper", "-v"]).decode("UTF-8").strip()
-date=subprocess.check_output(["date", "+%Y%m%d"]).decode("UTF-8").strip()
-tags=subprocess.check_output(["git", "tag"]).decode("UTF-8").strip().split("\n")
-tag=version + "-" + date
+version = subprocess.check_output(["bin/helper", "-v"]).decode("UTF-8").strip()
+date = subprocess.check_output(["date", "+%Y%m%d"]).decode("UTF-8").strip()
+tags = subprocess.check_output(["git", "tag"]).decode("UTF-8").strip().split("\n")
+tag = version + "-" + date
 
-fail=False
+fail = False
 
-if len(subprocess.check_output(["git", "status", "--porcelain"]).decode("UTF-8").strip()) > 0:
+
+def check_output(cmd):
+    return subprocess.check_output(cmd).decode("UTF-8").strip()
+
+    
+if len(check_output(["git", "status", "--porcelain"])) > 0:
     print("Folder not clean!")
-    print(subprocess.check_output(["git", "status", "--porcelain"]).decode("UTF-8").strip())
+    print(check_output(["git", "status", "--porcelain"]))
     print("\n")
-    fail=True
+    fail = True
 
 for i in tags:    
     if version in i:
-        print (f"Tag {version} already exists.")
+        print(f"Tag {version} already exists.")
         print("\n")
-        fail=True
+        fail = True
         
 with open("README.md") as inf:
-    readme=inf.read()
+    readme = inf.read()
     if tag not in readme:
-        print (f"Tag  {tag} not in 'README.md'.")
+        print(f"Tag  {tag} not in 'README.md'.")
         print("\n")
-        fail=True
+        fail = True
     
 if not fail:
-    subprocess.call(["git","tag",tag])
-    subprocess.call(["git","push"])
-    subprocess.call(["git","push","--tag"])
+    subprocess.call(["git", "tag", tag])
+    subprocess.call(["git", "push"])
+    subprocess.call(["git", "push", "--tag"])
     sys.exit(0)
 else:    
     sys.exit(1)
-            
-
-#print(version)
-#print(date)
-#print(tags)
